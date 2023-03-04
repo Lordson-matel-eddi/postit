01621cdbb405ee227cca49567154e9150bcbbd7a
lordson <lordsonmateleddi@gmail.com>
2023-03-04 10:57:39 +0100

    adding nextauth

--- prisma/client.js
+++ prisma/client.js
@@ -2,5 +2,5 @@ import { PrismaClient } from "@prisma/client";
 
 const client = global.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
-2
+
 export default client;
