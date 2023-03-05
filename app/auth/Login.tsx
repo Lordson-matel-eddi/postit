a643830bb5b35ea8ecee1416538641e52ddd4324
lordson <lordsonmateleddi@gmail.com>
2023-03-06 00:01:45 +0100

    cmt

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -5,7 +5,7 @@ import {signIn} from 'next-auth/react'
 export function Login(){
     return (
         <div className="">
-        <button  className="bg-slate-700 py-2 px-5 text-slate-200 rounded-lg text-xs uppercase tracking-wider shadow focus:ring focus: ring-slate-500 focus:ring-opacity-1 focus:ring-offset-1 hover:bg-slate-600">Login</button>
+        <button onClick={()=> signIn()} className="bg-slate-700 py-2 px-3 text-slate-200 rounded-lg text-xs tracking-wider shadow focus:ring focus: ring-slate-500 focus:ring-opacity-1 focus:ring-offset-1">Sign in</button>
         </div>
     )
 }
--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -5,6 +5,7 @@ import authOptions from '../api/auth/[...nextauth]'
 
 export default async function Navbar() {
     const session = await getServerSession(authOptions)
+    console.log(session)
     return (
         <nav className="h-16 px-5 flex justify-between items-center backdrop-blur-md border-b border-slate-300 text-slate-700">
             <Link href={"/"}>
